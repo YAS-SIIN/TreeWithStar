d7ea60280512d785027dbf722bc01652f5f4f219
Yasin Asadnezhad <yasin.asadnezhad@msn.com>
2023-04-14 17:15:14 +0430

    Work on class drawer and drawer tree method

---
+++ TreeWithStar/Drawer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeWithStar
+{
+    public class Drawer
+    {
+        public void Tree(int layer)
+        {
+            if (layer <= 0)
+            {
+                Console.WriteLine("please enter number bigger than zero");
+                return;
+            }
+            string row;
+            string[] arrStar = new string[layer];
+            arrStar[0] = "*";
+
+            for (int i = 1; i < arrStar.Length; i++)
+            {
+                for (int j = 0; j < arrStar[i - 1].Length + 2; j++)
+                {
+                    arrStar[i] += "*";
+                }
+            }
+
+            int lastRowCount = arrStar[arrStar.Length - 1].Length;
+            //for (int i = 0; i < arrStar.Length; i++)
+            //{
+            //    int spaceCount = (lastRowCount - arrStar[i].Length) / 2;
+            //    row = arrStar[i].PadLeft(spaceCount, '-');
+            //    Console.WriteLine(row);
+            //}
+                                                                       
+            for (int i = 0; i < arrStar.Length; i++)
+            {
+                int spaceCount = (lastRowCount - arrStar[i].Length) / 2;
+                row = "";
+                for (int k = 0; k < spaceCount; k++)
+                {
+                    row += " ";
+                }
+
+                row += arrStar[i];
+                Console.WriteLine(row);
+            }
+
+
+            Console.WriteLine("I".PadLeft((lastRowCount/2) + 1, ' '));
+        }
+    }
+}
--- TreeWithStar/Program.cs
+++ TreeWithStar/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using TreeWithStar;
+
+Drawer _drawer = new();
+_drawer.Tree(5);
+//Console.ReadLine();              
+
 Console.WriteLine("Hello, World!");
+             
